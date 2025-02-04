8085e8f87b90a4c46d2be199ccf9f7fa161ac993
thiagofr4nc4 <thiagofr4nc4@gmail.comm>
2025-02-03 23:43:44 -0300

    Criação de tela de login e esboço de formulário de login

--- clinica/src/app/app.component.ts
+++ clinica/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelaLoginComponent } from "./components/tela-login/tela-login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
